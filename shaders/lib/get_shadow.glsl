4e56f64618a46c2e584bd8c1338d763890f0763f
epcr <epcrmail@gmail.com>
2023-02-02 23:54:31 -0600

    Even more precise alignment

--- shaders/lib/get_shadow.glsl
+++ shaders/lib/get_shadow.glsl
@@ -1,14 +1,14 @@
 float getShadow(in vec3 position, in vec3 cameraPosition, in vec3 normal, in mat4 shadowProjection, in mat4 shadowModelView, in vec2 texcoord, in sampler2D shadowtex, in sampler2D noisetex, in float lightmapLight, in int time) {    
     float shadow = 1;
     // align position to grid
-    vec3 posNew = position + 0.1 * normal - ceil(cameraPosition) + cameraPosition;
+    vec3 posNew = position + EPSILON * normal - ceil(cameraPosition) + cameraPosition;
 
 
     for(int i = 0; i < SHADOW_MAP_RANGE; i++) {
-        posNew.y ++;
         if(texture(shadowtex, voxelize(posNew)).r < 1) {
             shadow = 0;
         }
+        posNew.y ++;
     }
     
     return shadow;
