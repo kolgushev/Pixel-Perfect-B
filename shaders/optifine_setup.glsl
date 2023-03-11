88fa147e99fe0595aaa877edeb7c49a665b20a64
epcr <epcrmail@gmail.com>
2023-03-11 06:40:27 -0600

    Don't clear anything

--- shaders/optifine_setup.glsl
+++ shaders/optifine_setup.glsl
@@ -1,11 +1,13 @@
 // use floats since they aren't capped at one (for easier color manipulation)
 /*
 const int colortex0Format = RGB16F;
+const bool colortex0Clear = false;
 
 const int colortex1Format = RGBA16F;
+const bool colortex1Clear = false;
 
 const int colortex2Format = RGB8;
-const bool colortex3Clear = false;
+const bool colortex2Clear = false;
 
 const int colortex3Format = RGB8_SNORM;
 const bool colortex3Clear = false;
