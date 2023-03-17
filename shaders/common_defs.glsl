4992337628c3453601b42b9130dc8456e60f692c
epcr <epcrmail@gmail.com>
2023-03-16 19:06:48 -0500

    Added option for entity shadow distance

--- shaders/common_defs.glsl
+++ shaders/common_defs.glsl
@@ -326,6 +326,7 @@ NOTE: Any color values that aren't multiplied by a color trasform (eg. RGB_to_AC
 
 const int shadowMapResolution = 4096; // [512 1024 2048 4096 8192]
 const float shadowDistance = 200.0; // [100.0 125.0 150.0 175.0 200.0 225.0 250.0 275.0 300.0]
+const float entityShadowDistanceMul = 0.3; // [0.1 0.2 0.3 0.4]
 
 #define SHADOW_DISTORTION 0.9 // [0.0 0.5 0.8 0.9 0.95 0.98]
 // 0:off 1:2× 2:4×
