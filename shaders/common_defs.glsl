42b96111d3a9c80383be6dbde346198e0ccd9144
epcr <epcrmail@gmail.com>
2023-05-09 20:01:00 -0500

    Added options for CLOUD_EXTENSION

--- shaders/common_defs.glsl
+++ shaders/common_defs.glsl
@@ -165,7 +165,7 @@ NOTE: Any color values that aren't multiplied by a color trasform (eg. RGB_to_AC
 #define RAIN_TRANSPARENCY 0.5 // [0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0]
 #define SNOW_OPACITY 1
 
-#define CLOUD_EXTENSION 1.0
+#define CLOUD_EXTENSION 1.0 // [0.8 0.9 1.0 1.5 2.0 3.0 5.0 7.0 10.0]
 
 // #define FOG_ENABLED_USER
 #ifdef FOG_ENABLED_USER
