de1355ecddce0183b1b261abad2f19fe28d52970
epcr <epcrmail@gmail.com>
2023-07-11 22:50:22 -0500

    improved build using tar

--- build.bat
+++ build.bat
@@ -4,24 +4,13 @@ setlocal
 REM Set the original folder path
 set "original_folder=%~dp0"
 
-REM Escape backslashes in path
-@REM set "original_folder=%original_folder:\=\\%"
-
 REM Set the build directory path
 set "build_directory=%original_folder%..\build\"
-
-REM Set the unzipped directory
-set "unzipped_directory=%build_directory%unzipped\"
 
 REM Create the build directory if it doesn't exist
 if not exist "%build_directory%" (
     mkdir "%build_directory%"
 )
-
-REM Copy the contents of the original folder to the build directory
-xcopy "%original_folder%" "%unzipped_directory%" /s /e /i /y /q /exclude:%original_folder%exclude_from_build.txt
-
-echo Copied original folder to build directory
 
 REM Create a timestamp for the zip file
 set "timestamp=%date:/=-%_%time::=-%"
@@ -32,9 +21,9 @@ REM Set the zip file
 set "zip_file=%build_directory%Pixel-Perfect-%timestamp%.zip"
 
 REM Create the zip file
-powershell -nologo -noprofile -command "Add-Type -A 'System.IO.Compression.FileSystem'; [System.IO.Compression.ZipFile]::CreateFromDirectory('%unzipped_directory%', '%zip_file%')"
+tar.exe -a -c -f %zip_file% -X exclude_from_build.txt LICENSE shaders
 
-echo Created zip file
+echo Created zip file from "%unzipped_directory%"
 
 REM Replace the latest build file with current build
 copy "%zip_file%" "%original_folder%..\Pixel-Perfect-Latest.zip" /b /y
@@ -55,4 +44,3 @@ if not "%~1"=="/k" (
 )
 
 echo Build completed.
-pause
