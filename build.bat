74f35d7005f7a26ede04470b4e1adf96345a0528
epcr <epcrmail@gmail.com>
2023-07-11 22:00:58 -0500

    Made keeping files in build directory a parameter

--- build.bat
+++ build.bat
@@ -8,10 +8,10 @@ REM Escape backslashes in path
 @REM set "original_folder=%original_folder:\=\\%"
 
 REM Set the build directory path
-set "build_directory=%original_folder%..\build"
+set "build_directory=%original_folder%..\build\"
 
 REM Set the unzipped directory
-set "unzipped_directory=%build_directory%\unzipped"
+set "unzipped_directory=%build_directory%unzipped\"
 
 REM Create the build directory if it doesn't exist
 if not exist "%build_directory%" (
@@ -29,7 +29,7 @@ set "timestamp=%timestamp: =0%"
 set "timestamp=%timestamp:.=_%"
 
 REM Set the zip file
-set "zip_file=%build_directory%\Pixel-Perfect-%timestamp%.zip"
+set "zip_file=%build_directory%Pixel-Perfect-%timestamp%.zip"
 
 REM Create the zip file
 powershell -nologo -noprofile -command "Add-Type -A 'System.IO.Compression.FileSystem'; [System.IO.Compression.ZipFile]::CreateFromDirectory('%unzipped_directory%', '%zip_file%')"
@@ -37,13 +37,22 @@ powershell -nologo -noprofile -command "Add-Type -A 'System.IO.Compression.FileS
 echo Created zip file
 
 REM Replace the latest build file with current build
-copy "%zip_file%" "%build_directory%\Pixel-Perfect-Latest.zip" /b /y
+copy "%zip_file%" "%original_folder%..\Pixel-Perfect-Latest.zip" /b /y
 
 echo Replaced latest build
 
 REM Delete the unzipped directory
 rmdir "%unzipped_directory%" /s /q
 echo Deleted "%unzipped_directory%"
+
+if not "%~1"=="/k" (
+	REM Delete the original zip file
+	del "%zip_file%"
+	echo Deleted "%zip_file%"
+	echo You can use /k if you want to keep the zip file in the build folder after the program is done
+) else (
+	echo Kept "%zip_file%"
+)
 
 echo Build completed.
 pause
