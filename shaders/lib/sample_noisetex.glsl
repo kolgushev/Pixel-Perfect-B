707efa06c45c624dd4d6227f4c37b020a9bc1387
epcr <epcrmail@gmail.com>
2023-08-02 01:31:51 -0500

    Fix merge bug

--- shaders/lib/sample_noisetex.glsl
+++ shaders/lib/sample_noisetex.glsl
@@ -15,5 +15,5 @@ vec4 tile(in vec2 texcoord, in int id, in bool sharp) {
 		texcoord /= NOISETEX_TILES_RES;
 	}
 	
-	return texture3D(noisetex, vec3(texcoord, removeBorder(float(id) / float(NOISE_LAYER_COUNT - 1), 1.0 / float(NOISE_LAYER_COUNT))));
+	return texture(noisetex, vec3(texcoord, removeBorder(float(id) / float(NOISE_LAYER_COUNT - 1), 1.0 / float(NOISE_LAYER_COUNT))));
 }
