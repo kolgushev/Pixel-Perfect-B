2e22e137fc55c3d6dac2b5cd1350b2eaf4e8751a
epcr <epcrmail@gmail.com>
2023-08-02 15:09:29 -0500

    Removed sky mixing

--- shaders/lib/atmospherics/pixel_perfect_sky.glsl
+++ shaders/lib/atmospherics/pixel_perfect_sky.glsl
@@ -5,9 +5,6 @@ vec3 pixelPerfectSkyVector(vec3 v, vec3 sun_dir, vec2 stars, float rain, float s
 	vec3 vec = normalize(vec3(v.x, mix(0.03, vy, smoothstep(0.03, 0.1, vy)), v.z));
 	vec3 color = hosekWilkieSkyVector(vec, sun_dir);
 
-	#define A mix(-0.05, -0.2, abs(skyTime))
-	color = mix(skyAlbedo * XYZ_to_ACEScg * color, color, smoothstep(A, 0.07, v.y));
-
 	float mixFactor = smoothstep(THUNDER_THRESHOLD, 1, rain) * skyTime;
 	color = mix(color, RAINY_SKY_COLOR, mixFactor);
         
