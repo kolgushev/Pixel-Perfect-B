9039a0eab97ed976ebb5b8a0dbdaec5691891ead
epcr <epcrmail@gmail.com>
2024-08-02 02:12:35 -0500

    Improved stranslucent shadow falloff

--- shaders/lib/shading/get_shadow.glsl
+++ shaders/lib/shading/get_shadow.glsl
@@ -60,32 +60,40 @@ float getShadow(in vec3 position, in vec3 normal, in mat3 TBN, in vec2 screenPos
             #endif
 
             float radius = SHADOW_FILTERING_RADIUS;
-            vec2 noise = vec2(0.0);
+            vec3 noise = vec3(0.0);
 
             #if SHADOW_FILTERING == FILTERING_PCF && defined DO_SUBSURFACE
                 if(isSubsurf && isUnlit) {
-                    radius = 0.5 * subsurface;
+                    radius = subsurface;
                 }
             #elif SHADOW_FILTERING == FILTERING_BILINEAR
                 if(isSubsurf && isUnlit) {
                     radius = subsurface;
             #endif
-                vec2 offset;
+                vec3 offset;
                 vec3 worldOffset;
                 vec3 samplePos;
+                float sampleShadow;
                 // Generate sample positions in tangent space using blue noise
                 for (int i = 0; i < SHADOW_FILTERING_SAMPLES; i++) {
-                    noise = tile(screenPos + i * (113.0 - EPSILON) + randomVec.xy * 256.0, NOISE_WHITE_4D, true).xy;
-                    offset = radius * (noise.xy * 2.0 - 1.0);
-                    
+                    noise = tile(screenPos + i * (113.0 - EPSILON) + randomVec.xy * 256.0, NOISE_BLUE_3D, true).xyz;
+
+                    if(isSubsurf) {
+                        offset = radius * (noise * 2.0 - 1.0);
+                    } else {
+                        offset = radius * vec3(noise.xy * 2.0 - 1.0, 0.0);
+                    }
+
                     // transform offset to world space using TBN matrix
-                    worldOffset = TBN * vec3(offset, 0.0);
+                    worldOffset = TBN * offset;
                     
                     // apply offset to shadow position
                     samplePos = shadowPosition + worldOffset;
 
                     samplePos = toClipspace(shadowProjection, shadowModelView, samplePos).xyz;
-                    shadow += shadowStep(shadowSample(samplePos), subsurface, factor);
+                    sampleShadow = shadowStep(shadowSample(samplePos), subsurface, factor);
+
+                    shadow += sampleShadow;
                 }
 
                 shadow *= 1.0 / float(SHADOW_FILTERING_SAMPLES);
