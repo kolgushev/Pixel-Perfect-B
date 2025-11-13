858e674abf210fe3ff51fd6f9d52eb29240874b1
Daniel Kolgushev <kolgushevd@gmail.com>
2025-11-13 16:59:43 -0500

    Fix particles in newer versions

--- shaders/optifine_setup.glsl
+++ shaders/optifine_setup.glsl
@@ -102,10 +102,10 @@ const float drynessHalflife = 70.0;
 #if defined g_skybasic || defined g_skytextured
     #define gc_sky
 #endif
-#if defined g_textured || defined g_textured_lit || defined g_particles
+#if defined g_textured || defined g_textured_lit
     #define gc_textured
 #endif
-#if defined g_water || defined g_dh_water || defined g_hand_water || defined g_weather || defined g_clouds || defined gc_textured || defined g_particles_translucent
+#if defined g_water || defined g_dh_water || defined g_hand_water || defined g_weather || defined g_clouds || defined gc_textured || defined g_particles_translucent || defined g_particles
     #define gc_transparent
 #endif
 #if defined g_weather || defined g_clouds || defined gc_textured || defined g_particles_translucent
